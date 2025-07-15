a663691f2c1d00a8b1a456050e39ac19a691179a
Eliecer Bedoya <eliecerbedoya7@gmail.com>
2025-07-15 16:16:03 -0400

    integrando adminlte

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
     <title>@yield('title', 'Sistema de Gestión')</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @stack('styles')
 </head>
-<body class="hold-transition sidebar-mini">
+<body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
 
     @include('partials.navbar')
     @include('partials.sidebar')
 
-    <div class="content-wrapper p-4">
-        @yield('content')
+    <div class="content-wrapper">
+        <section class="content pt-3 px-3">
+            @yield('content')
+        </section>
     </div>
 
     @include('partials.footer')
 
 </div>
-
-<script src="{{ asset('js/app.js') }}"></script>
 @stack('scripts')
 </body>
 </html>
--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,13 +1,10 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Logo -->
     <a href="#" class="brand-link">
         <img src="{{ asset('images/logo.png') }}" alt="Logo" class="brand-image img-circle elevation-3">
         <span class="brand-text font-weight-light">INTELCOL</span>
     </a>
 
-    <!-- Menú -->
     <div class="sidebar">
-        <!-- Usuario actual -->
         <div class="user-panel mt-3 pb-3 mb-3 d-flex">
             <div class="image">
                 <img src="{{ asset('images/user.png') }}" class="img-circle elevation-2" alt="User">
@@ -17,7 +14,6 @@
             </div>
         </div>
 
-        <!-- Navegación -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
                 <li class="nav-item">
